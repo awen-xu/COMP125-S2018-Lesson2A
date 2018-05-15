7de81d0dd379a0311145b3c1ecf59c4642b8aea2
awen-xu <wxu59@my.centennialcollege.ca>
2018-05-15 14:25:21 -0400

    event listener for about button

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,10 +3,26 @@
 // IIFE - Immediately Invoked Function Expression
 // anonymous self executing function
 (function (){
+    "use strict"
 
     function Start(){
         // local variable
-        var title = document.title;
+        let title = document.title;
+
+        switch(title){
+            case "COMP125 - Home":
+                let AboutButton = document.getElementById("AboutButton");
+
+                AboutButton.addEventListener("click", function(){
+                    console.log("About Button was clicked!")
+                });
+            break;
+            
+            case "COMP125 - About":
+            break;
+        }
+
+        
 
 
 
